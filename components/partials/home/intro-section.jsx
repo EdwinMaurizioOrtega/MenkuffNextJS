0bb24dd313871b7672e7e12ad69831f971d6f8aa
Edwin Ortega <edwin.maurizio.ortega@gmail.com>
2022-10-06 11:56:00 -0500

    7to Menkuff

--- components/partials/home/intro-section.jsx
+++ components/partials/home/intro-section.jsx
@@ -14,53 +14,53 @@ function IntroSection ( props ) {
         <div className="row">
             <div className="col-lg-9 mb-4">
                 <OwlCarousel adClass="owl-theme owl-dot-inner intro-slider animation-slider" options={ introSlider }>
-                    {/*<div className="banner banner-fixed content-middle intro-slide intro-slide1 banner-radius" style={ { backgroundColor: "#f8f8f8" } }>*/}
-                    {/*    <figure>*/}
-                    {/*        <LazyLoadImage*/}
-                    {/*            src="images/home/slides/1.jpg"*/}
-                    {/*            alt="Banner"*/}
-                    {/*            width="1030"*/}
-                    {/*            height="450"*/}
-                    {/*        />*/}
-                    {/*    </figure>*/}
-                    {/*    <div className="banner-content">*/}
-                    {/*        <Reveal keyframes={ fadeInLeftShorter } duration={ 1000 }>*/}
-                    {/*            <h5 className="banner-subtitle text-capitalize font-weight-normal">Conoce más sobre la emisión de obligaciones</h5>*/}
-                    {/*            <h3 className="banner-title text-uppercase font-weight-bold ls-m">Resolución No.</h3>*/}
-                    {/*            <div className="banner-price-info font-weight-semi-bold text-body text-uppercase ls-m">*/}
-                    {/*                SCVS-INMV-DNAR-2022-00003181*/}
-                    {/*            </div>*/}
-                    {/*            <ALink href="https://apollo.hipertronics.us/uploads/files/RESOLUCION_3189_I_EOLP_LIDENAR_SA.pdf" className="btn btn-dark btn-outline btn-rounded">*/}
-                    {/*                Descargar Aquí<i className="d-icon-arrow-right"></i></ALink>*/}
-                    {/*        </Reveal>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
-                    <div className="banner banner-fixed content-middle intro-slide intro-slide2 banner-radius" style={ { backgroundColor: "#e2e2e3" } }>
+                    <div className="banner banner-fixed content-middle intro-slide intro-slide2 banner-radius" style={ { backgroundColor: "#f8f8f8" } }>
                         <figure>
                             <LazyLoadImage
-                                src="images/home/slides/2.jpg"
+                                src="images/home/slides/1.png"
                                 alt="Banner"
                                 width="1030"
                                 height="450"
                             />
                         </figure>
                         <div className="banner-content text-right">
+                            <Reveal keyframes={ fadeInLeftShorter } duration={ 1000 }>
+                                {/*<h5 className="banner-subtitle text-capitalize font-weight-normal">Conoce más sobre la emisión de obligaciones</h5>*/}
+                                <h3 className="banner-subtitle text-capitalize font-weight-normal">REDMI 10 2022</h3>
+                                {/*<div className="banner-price-info font-weight-semi-bold text-body text-uppercase ls-m">*/}
+                                {/*    SCVS-INMV-DNAR-2022-00003181*/}
+                                {/*</div>*/}
+                                {/*<ALink href="https://apollo.hipertronics.us/uploads/files/RESOLUCION_3189_I_EOLP_LIDENAR_SA.pdf" className="btn btn-dark btn-outline btn-rounded">*/}
+                                {/*    Descargar Aquí<i className="d-icon-arrow-right"></i></ALink>*/}
+                            </Reveal>
+                        </div>
+                    </div>
+                    <div className="banner banner-fixed content-middle intro-slide intro-slide2 banner-radius" style={ { backgroundColor: "#e2e2e3" } }>
+                        <figure>
+                            <LazyLoadImage
+                                src="images/home/slides/2.png"
+                                alt="Banner"
+                                width="1030"
+                                height="450"
+                            />
+                        </figure>
+                        <div className="banner-content ">
                             <Reveal keyframes={ fadeInRightShorter } duration={ 1000 }>
-                                <h5 className="banner-subtitle text-capitalize font-weight-normal">¡Llévalo donde quieras!</h5>
-                                <h3 className="banner-title text-uppercase font-weight-bold ls-m">The Freestyle</h3>
-                                <div className="banner-price-info font-weight-semi-bold text-dark text-uppercase ls-m">
-                                    Portátil <span className="text-primary">Poderoso</span>
-                                </div>
-                                <p className="text-dark font-weight-normal">Sorprendentemente simple</p>
-                                <ALink href="/shop" className="btn btn-dark btn-outline btn-rounded">
-                                    Comprar<i className="d-icon-arrow-right"></i></ALink>
+                                {/*<h5 className="banner-subtitle text-capitalize font-weight-normal">¡Llévalo donde quieras!</h5>*/}
+                                <h5 className="banner-subtitle text-capitalize font-weight-normal">Galaxy S22 Ultra</h5>
+                                {/*<div className="banner-price-info font-weight-semi-bold text-dark text-uppercase ls-m">*/}
+                                {/*    Portátil <span className="text-primary">Poderoso</span>*/}
+                                {/*</div>*/}
+                                {/*<p className="text-dark font-weight-normal">Sorprendentemente simple</p>*/}
+                                {/*<ALink href="/shop" className="btn btn-dark btn-outline btn-rounded">*/}
+                                {/*    Comprar<i className="d-icon-arrow-right"></i></ALink>*/}
                             </Reveal>
                         </div>
                     </div>
                     <div className="banner banner-fixed content-middle intro-slide intro-slide3 banner-radius" style={ { backgroundColor: "#d8dee4" } }>
                         <figure>
                             <LazyLoadImage
-                                src="images/home/slides/3.jpg"
+                                src="images/home/slides/3.png"
                                 alt="Banner"
                                 width="1030"
                                 height="450"
@@ -68,13 +68,13 @@ function IntroSection ( props ) {
                         </figure>
                         <div className="banner-content">
                             <Reveal keyframes={ fadeInLeftShorter } duration={ 1000 }>
-                                <h5 className="banner-subtitle text-capitalize font-weight-normal mb-1">Tu TV ideal</h5>
-                                <h3 className="banner-title text-uppercase font-weight-bold ls-m">Smart TV</h3>
-                                <div className="banner-price-info font-weight-semi-bold text-dark text-uppercase ls-m">
-                                    Brillante y colorido, <span className="text-primary">QLED 4k</span>
-                                </div>
-                                <p className="text-dark font-weight-normal">Explora cual de nuestras TVs, se adapta más a ti</p>
-                                <ALink href="/shop" className="btn btn-dark btn-outline btn-rounded">Comprar<i className="d-icon-arrow-right"></i></ALink>
+                                {/*<h5 className="banner-subtitle text-capitalize font-weight-normal mb-1">Tu TV ideal</h5>*/}
+                                <h3 className="banner-subtitle text-capitalize font-weight-normal"> Galaxy Z Flip4</h3>
+                                {/*<div className="banner-price-info font-weight-semi-bold text-dark text-uppercase ls-m">*/}
+                                {/*    Brillante y colorido, <span className="text-primary">QLED 4k</span>*/}
+                                {/*</div>*/}
+                                {/*<p className="text-dark font-weight-normal">Explora cual de nuestras TVs, se adapta más a ti</p>*/}
+                                {/*<ALink href="/shop" className="btn btn-dark btn-outline btn-rounded">Comprar<i className="d-icon-arrow-right"></i></ALink>*/}
                             </Reveal>
                         </div>
                     </div>
